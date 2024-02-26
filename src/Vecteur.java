227388fd03cc00d10bf6f83630383cadec9ec0d0
David G <david.giasson@claurendeau.qc.ca>
2024-02-26 17:06:29 -0500

    Ajout des attributs et du constructeur de la classe Vecteur;

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,2 +1,11 @@
 public class Vecteur {
+    private int[] tableau;
+    private int nbElements;
+    private static final int RATIO_AGRANDISSEMENT = 2;
+    private static final int TAILLE_INITIALE = 5;
+
+    public Vecteur() {
+        tableau = new int[TAILLE_INITIALE];
+        nbElements = 0;
+    }
 }
