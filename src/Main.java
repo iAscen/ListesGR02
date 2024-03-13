8fba17b890e405985a19f9decfce24df917f3a26
David G <david.giasson@claurendeau.qc.ca>
2024-03-13 17:18:35 -0400

    Remaniement des données de test.

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
 
         System.out.println("Test 1: Ajout de valeurs dans le vecteur");
         data.ajouter(0);
-        data.ajouter(10);
-        data.ajouter(20);
+        data.ajouter(2);
+        data.ajouter(4);
         System.out.println(data);
 
         System.out.println("Test 2: Agrandir le vecteur quand il est plein");
-        data.ajouter(30);
-        data.ajouter(40);
-        data.ajouter(50);
+        data.ajouter(6);
+        data.ajouter(8);
+        data.ajouter(10);
         System.out.println(data);
 
         System.out.println("Test 3: Ajouter des valeurs à index spécifiques");
-        data.ajouter(-10, 0);
-        data.ajouter(25, 4);
-        data.ajouter(60, 8);
+        data.ajouter(-1, 0);
+        data.ajouter( 5, 4);
+        data.ajouter(11, 8);
         System.out.println(data);
 
         System.out.println("Test 4: Ajouter des valeurs provenant d'un autre vecteur");
